a4ec2c2119172bc1381e79539633624d3bfc6cf1
Julian Gonzalez <lkgrayfox@gmail.com>
2022-06-28 22:40:40 -0500

    Method Get

---
+++ DapperCrud/DapperCrud/Controllers/heroController.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using DapperCrud.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class heroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public heroController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Hero>>> GetAllSuperHeroes()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<Hero> heroes = await selectAllHeroes(connection);
+            return Ok(heroes);
+        }
+
+        [HttpGet("HeroId")]
+        public async Task<ActionResult<Hero>> GetHero(int HeroId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryFirstAsync<Hero>("select * from Hero where id= @id",
+                new { Id = HeroId });
+            return Ok(hero);
+        }
+
+        private static async Task<IEnumerable<Hero>> selectAllHeroes(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Hero>("select * from Hero");
+        }
+    }
+}
---
+++ DapperCrud/DapperCrud/Data/Entities/Hero.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperCrud.Data.Entities
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string NameHero { get; set; } = string.Empty;
+        public string FirstDate { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string place { get; set; } = string.Empty;
+    }
+}
